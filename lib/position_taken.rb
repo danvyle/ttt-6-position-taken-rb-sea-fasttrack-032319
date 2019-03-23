9b83df01d51bbcc99afcc0f554f24f5a4a4ac2b8
Dan-Vy Le <danvyle85@gmail.com>
2019-03-23 20:58:48 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,12 @@
 # code your #position_taken? method here!
 board = [" ", "X", " ", " ", " ", " ", " ", " ", " "]
 
-def position_taken(index_number)
-  if board[index_number] != " "
+def position_taken(index_number, gameboard)
+  if gameboard[index_number] != " "
     puts "Yes, it is taken"
   else
     puts "No, it is not taken"
+  end
 end
 
-position_taken
+position_taken(1, board)
