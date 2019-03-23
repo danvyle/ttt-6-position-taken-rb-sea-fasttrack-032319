0a84d939b02004eb288cc5da9d489cf4c21d560d
Dan-Vy Le <danvyle85@gmail.com>
2019-03-23 21:14:25 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,15 +1,17 @@
 # code your #position_taken? method here!
 board = [" ", "X", " ", " ", " ", " ", " ", " ", " "]
 
+user_input = gets.chomp.to_i
+
+index = user_input - 1 
+
 def position_taken? (board, index)
   if board[index] == " " || "" || "nil"
   end
 end
 
 
-user_input = gets.chomp.to_i
 
-index = user_input - 1 
 
 
 position_taken?(board, index)
