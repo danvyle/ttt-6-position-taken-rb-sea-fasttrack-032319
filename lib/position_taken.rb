eee6b719848da3a2be70a63e4c0a586f72f2eb77
Dan-Vy Le <danvyle85@gmail.com>
2019-03-23 20:20:30 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,11 @@
 # code your #position_taken? method here!
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-def position_taken?
-  if array[index_number] != " "
-    puts "Yes, it is taken"
-  else
-    puts "No, it is not taken"
-end
+# def position_taken?
+#   if array[index_number] != " "
+#     puts "Yes, it is taken"
+#   else
+#     puts "No, it is not taken"
+# end
 
-puts position_taken 
+# puts position_taken 
